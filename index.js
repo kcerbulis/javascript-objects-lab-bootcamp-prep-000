9d8cf7c1425b07223773e8b2b4642ec355c120bf
reactive-queue-1834 <kcerbulis@gmail.com>
2019-02-10 23:23:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var recipes = new Object()
 
 
 function updateObjectWithKeyAndValue(object, key, value){
-  Object.assign ({}, object)
+  var obj1 = Object.assign ({}, object)
   object[key] = value
   return object
 }
