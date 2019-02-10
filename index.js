51d3633841e2130d5863855152e343d419e3a93d
reactive-queue-1834 <kcerbulis@gmail.com>
2019-02-10 22:33:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object
 }
+
+
+var newRecipes = Object.assign({}, recipes)
+
+function deleteFromObjectByKey(object, key){
+  
+}
